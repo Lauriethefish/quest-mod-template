ba02c713709d27b252ff1960a004478c8eb09bb8
Dan Rouse <dan@rouse.house>
2020-11-21 13:22:52 -0500

    call build.ps1 from other scripts
    modularizes buildBMBF and copy by calling build.ps1 instead of duplicating build args

--- copy.ps1
+++ copy.ps1
@@ -1,10 +1,3 @@
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
-
-$buildScript = "$NDKPath/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
-}
-
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+& $PSScriptRoot/build.ps1
 & adb push libs/arm64-v8a/lib#{id}_0_1_0.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/lib#{id}_0_1_0.so
 & adb shell am force-stop com.beatgames.beatsaber
