05a488447b572e512d1f3408ab70eb1d0dbb6a66
Dan Rouse <dan@rouse.house>
2020-11-21 13:25:56 -0500

    restart beatsaber after copy

--- copy.ps1
+++ copy.ps1
@@ -3,5 +3,6 @@ if ($?) {
     adb push libs/arm64-v8a/lib#{id}.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/lib#{id}.so
     if ($?) {
         adb shell am force-stop com.beatgames.beatsaber
+        adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
     }
 }
