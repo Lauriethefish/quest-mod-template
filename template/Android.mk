da8e5d28e8a1a75f9bdd37203977e253339722c5
Laurie Fay <lauriethefish@outlook.com>
2021-01-08 10:52:59 +0000

    Updated BS-hook version.

--- template/Android.mk
+++ template/Android.mk
@@ -27,9 +27,9 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_0_8_2
+LOCAL_MODULE := beatsaber-hook_1_0_4
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_8_2.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_4.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -39,7 +39,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_8_2
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_4
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"#{id}"' -DVERSION='"0.1.0"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
--- template/buildBMBF.ps1
+++ template/buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so", "./bmbfmod.json" -DestinationPath "./#{id}_v0.1.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_4.so", "./bmbfmod.json" -DestinationPath "./#{id}_v0.1.0.zip" -Update
 }
