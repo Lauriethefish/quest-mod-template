7cc570316e80e13146755b998e39067997f8364f
Lauriethefish <lauriethefish@outlook.com>
2021-03-27 10:38:22 +0000

    Add QMOD build support

---
+++ template/buildQP.ps1
@@ -0,0 +1,14 @@
+# Builds a .qmod file for loading with QuestPatcher
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+$ArchiveName = "#{id}_v0.1.0.qmod"
+$TempArchiveName = "#{id}_v0.1.0.qmod.zip"
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "libbeatsaber-hook_1_2_4.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Move-Item $TempArchiveName $ArchiveName -Force
