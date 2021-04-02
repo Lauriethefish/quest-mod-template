51a49f88a0c8149db607ffa47ce9ab8a1b7c6f84
Lauriethefish <lauriethefish@outlook.com>
2021-04-02 17:56:24 +0100

    Fix typo

--- makeTemplate.ps1
+++ makeTemplate.ps1
@@ -1,3 +1,3 @@
 Remove-Item quest-mod-template.zip -ErrorAction Ignore
 Compress-Archive template/* quest-mod-template.zip
-Write-Output "Template saved to template.zip"
+Write-Output "Template saved to quest-mod-template.zip"
