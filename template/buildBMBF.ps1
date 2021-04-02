4b7bb0a92dbcfeb459351487be2bc858dc0c20c2
Lauriethefish <lauriethefish@outlook.com>
2021-04-02 17:48:08 +0100

    Remove legacy BMBF ZIP build

--- template/buildBMBF.ps1
+++
@@ -1,6 +0,0 @@
-# Builds a .zip file for loading with BMBF
-& $PSScriptRoot/build.ps1
-
-if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./bmbfmod.json" -DestinationPath "./#{id}_v0.1.0.zip" -Update
-}
