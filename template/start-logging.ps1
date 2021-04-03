79ad84ed5fd306a6d8174fc1c5cea87357b87c11
EnderdracheLP <30808481+EnderdracheLP@users.noreply.github.com>
2021-04-03 18:47:43 +0200

    Add --file parameter, with live output
    Added, a --file parameter that saves the output to file, while also showing it.

--- template/start-logging.ps1
+++ template/start-logging.ps1
@@ -4,4 +4,11 @@ while ([string]::IsNullOrEmpty($bspid)) {
     Start-Sleep -Milliseconds 100
     $bspid = adb shell pidof com.beatgames.beatsaber
 }
-adb logcat -T "$timestamp" --pid $bspid | Select-String -pattern "(QuestHook|modloader|AndroidRuntime)"
+if ($args.Count -eq 0) {
+    echo "Start logging!"
+    adb logcat -T "$timestamp" --pid $bspid | Select-String -pattern "(QuestHook|modloader|AndroidRuntime)"
+}
+if ($args[0] -eq "--file") {
+     echo "Logging and saving to file!"
+     (adb logcat -T "$timestamp" --pid $bspid | Select-String -pattern "(QuestHook|modloader|AndroidRuntime)") | Tee-Object -FilePath $PSScriptRoot/logcat.log
+}
