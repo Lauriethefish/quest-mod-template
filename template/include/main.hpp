e6aa1a59de5af2a5d4fe31633c5b794b150a0df6
rui2015 <15705566+rui2015@users.noreply.github.com>
2021-09-13 21:27:58 +0100

    Add hooking macros

--- template/include/main.hpp
+++ template/include/main.hpp
@@ -8,6 +8,7 @@
 #include "beatsaber-hook/shared/utils/logging.hpp"
 #include "beatsaber-hook/shared/config/config-utils.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+#include "beatsaber-hook/shared/utils/hooking.hpp"
 
 // Define these functions here so that we can easily read configuration and log information from other files
 Configuration& getConfig();
