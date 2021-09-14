2db98176e6a23dd96b2e3e26613f09507ee653ec
Millzy <64277238+MillzyDev@users.noreply.github.com>
2021-09-14 21:51:07 +0100

    added stopwatch

--- template/buildQMOD.ps1
+++ template/buildQMOD.ps1
@@ -1,14 +1,28 @@
+$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+
 # Builds a .qmod file for loading with QuestPatcher
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$Version = "0.1.4"
 
 $buildScript = "$NDKPath/build/ndk-build"
 if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "#{id}_v0.1.0.qmod"
-$TempArchiveName = "#{id}_v0.1.0.qmod.zip"
+$ArchiveName = "ImageCoverExpander-$Version.qmod"
+$TempArchiveName = "ImageCoverExpander-$Version.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "./libs/arm64-v8a/libbeatsaber-hook_2_0_3.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+
+$stopwatch.Stop()
+$timeElapsed = [math]::Round($stopwatch.Elapsed.TotalSeconds,3)
+Write-Output "SO build completed in $timeElapsed seconds"
+$stopwatch.Start()
+
+Compress-Archive -Path "./libs/arm64-v8a/libImageCoverExpander.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "imagecoverexpander.png", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
+
+$stopwatch.Stop()
+$timeElapsed = [math]::Round($stopwatch.Elapsed.TotalSeconds,3)
+Write-Output "QMOD build completed in $imeElapsed seconds"
