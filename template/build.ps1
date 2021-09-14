5f466dc383afdba1dfc97d115c2b90cf2ffcb985
Millzy <64277238+MillzyDev@users.noreply.github.com>
2021-09-14 21:51:37 +0100

    added stopwatch

--- template/build.ps1
+++ template/build.ps1
@@ -1,3 +1,5 @@
+$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
 $buildScript = "$NDKPath/build/ndk-build"
@@ -7,4 +9,7 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 
+$stopwatch.Stop()
+$timeElapsed = [math]::Round($stopwatch.Elapsed.TotalSeconds,3)
+Write-output "SO build completed in $timeElapsed seconds"
 Exit $LASTEXITCODE
