0757d49151ff3675b7730bd61970232847ea43fb
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2021-12-26 14:32:20 +0000

    Added Clean switch to the copy script

--- template/copy.ps1
+++ template/copy.ps1
@@ -1,6 +1,12 @@
 Param(
     [Parameter(Mandatory=$false)]
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
     [Switch] $log,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $useDebug,
 
     [Parameter(Mandatory=$false)]
     [Switch] $self,
@@ -15,16 +21,14 @@ Param(
     [Switch] $file,
 
     [Parameter(Mandatory=$false)]
-    [Switch] $help,
-
-    [Parameter(Mandatory=$false)]
-    [Switch] $useDebug
+    [Switch] $help
 )
 
 if ($help -eq $true) {
     echo "`"Copy`" - Builds and copies your mod to your quest, and also starts Beat Saber with optional logging"
     echo "`n-- Arguments --`n"
 
+    echo "-Clean `t`t Performs a clean build (equvilant to running `"Build -clean`")"
     echo "-UseDebug `t Copied the debug version of the mod to your quest"
     echo "-Log `t`t Logs Beat Saber using the `"Start-Logging`" command"
 
@@ -35,7 +39,7 @@ if ($help -eq $true) {
     exit
 }
 
-& $PSScriptRoot/build.ps1
+& $PSScriptRoot/build.ps1 -clean:$clean
 
 if ($LASTEXITCODE -ne 0) {
     echo "Failed to build, exiting..."
