dcdd1760e19ce95ac54802ecaa59f377f9dcffc8
Metalit <kylesmithhanna@gmail.com>
2022-05-11 20:59:02 -0500

    Remove redundant config

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -2,13 +2,6 @@
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
-// Loads the config from disk using our modInfo, then returns it for use
-Configuration& getConfig() {
-    static Configuration config(modInfo);
-    config.Load();
-    return config;
-}
-
 // Returns a logger, useful for printing debug messages
 Logger& getLogger() {
     static Logger* logger = new Logger(modInfo);
@@ -21,7 +14,6 @@ extern "C" void setup(ModInfo& info) {
     info.version = VERSION;
     modInfo = info;
 	
-    getConfig().Load(); // Load the config file
     getLogger().info("Completed setup!");
 }
 
