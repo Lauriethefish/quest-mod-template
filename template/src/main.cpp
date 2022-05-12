aa3ed47cd52ad864c772ef67bb1ed333494d04c0
Metalit <kylesmithhanna@gmail.com>
2022-05-11 21:01:01 -0500

    Improve intellisense

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -10,7 +10,7 @@ Logger& getLogger() {
 
 // Called at the early stages of game loading
 extern "C" void setup(ModInfo& info) {
-    info.id = MOD_ID;
+    info.id = ID;
     info.version = VERSION;
     modInfo = info;
 	
