67b37ac9f4d90fefd89e3779d0006cb61e8005c3
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-03-03 17:32:13 -0400

    Move -O3 to release mode

--- template/cmake/release.cmake
+++ template/cmake/release.cmake
@@ -1,6 +1,9 @@
 include_guard()
 
 if(${CMAKE_BUILD_TYPE} STREQUAL "RELEASE" OR ${CMAKE_BUILD_TYPE} STREQUAL "RelWithDebInfo")
+    # Better optimizations
+    add_compile_options(-O3)
+    
     # LTO
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
     add_compile_options(-flto)
